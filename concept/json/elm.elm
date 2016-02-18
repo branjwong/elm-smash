f398602cd0167725b3f696f14ea8a54f0c3582fb
branjwong <bjwong@sfu.ca>
2016-02-18 01:23:19 -0800

    removed stupid stuff, work on figuring out json

--- concept/json/elm.elm
+++ concept/json/elm.elm
@@ -3,11 +3,13 @@ module Client where
 import Html exposing (text)
 import Json.Decode exposing (Decoder, decodeString
                             , string, int, float
-                            , tuple2, (:=)
+                            , object2, (:=)
                             )
-import Result
 
-jsonString = "{ \"x\": 2, \"x\":4 }"
+jsonString = "{ \"x\": 2, \"y\":4 }"
+
+type alias Point = { x : Int , y : Int }
+
 
 x : Decoder Int
 x = 
@@ -19,15 +21,41 @@ y =
 
 coordinate : Decoder (Int, Int)
 coordinate = 
-  tuple2 (,) x y
+  object2 (,) x y
 
 elmJson : Result String (Int, Int)
 elmJson = decodeString coordinate jsonString
 
 parsedJson : (Int, Int)
-parsedJson = Result.withDefault (0,0) elmJson 
+parsedJson = Result.withDefault (0,0) elmJson
 
 stringJson : String
 stringJson = toString parsedJson
 
-main = text stringJson
+--
+
+elmJsonSingle : Result String Int
+elmJsonSingle = decodeString x jsonString
+
+parsedJsonSingle : Int
+parsedJsonSingle = Result.withDefault 0 elmJsonSingle
+
+stringJsonSingle : String
+stringJsonSingle = toString parsedJsonSingle
+
+--
+
+point : Decoder Point
+point = 
+  object2 Point x y
+
+elmJsonPoint : Result String Point
+elmJsonPoint = decodeString point jsonString
+
+parsedJsonPoint : Point
+parsedJsonPoint = Result.withDefault { x = 0, y = 0 } elmJsonPoint
+
+stringJsonPoint : String
+stringJsonPoint = toString parsedJsonPoint
+
+main = text stringJsonPoint
